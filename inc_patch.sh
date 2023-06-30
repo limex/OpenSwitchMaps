b574af4f37dc1654982a35854c628a7695df4d01
limex <guebosch@gmail.com>
2023-06-30 18:59:46 +0200

    WTF

--- inc_patch.sh
+++ inc_patch.sh
@@ -8,7 +8,7 @@ version=$(sed -n 's/.*"version": "\(.*\)",/\1/p' $manifest_file)
 a=(${version//./ })
 ((a[3]++))
 new_version="${a[0]}.${a[1]}.${a[2]}.${a[3]}"
-sed -i "s/\"version\": \"$version\"/\"version\": \"$new_version\"/g"$manifest_file
+# sed -i "s/\"version\": \"$version\"/\"version\": \"$new_version\"/g" $manifest_file
 
 # Increment version number in package.json
 npm version patch
